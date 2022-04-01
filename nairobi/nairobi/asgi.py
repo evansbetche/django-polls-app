2f5e3515686520da8f8e6f08680ca36f27c99698
Evansbettech <ebcheru@gmail.com>
2022-04-01 08:18:06 +0300

    All django files

---
+++ nairobi/nairobi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for nairobi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nairobi.settings')
+
+application = get_asgi_application()
